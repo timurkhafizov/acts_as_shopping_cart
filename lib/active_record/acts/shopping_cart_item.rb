92f0f3106134eef5bb3620756bc7ed845b35bbef
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-09-12 17:31:40 +0400

    Fix invalid currency on shopping cart item

--- lib/active_record/acts/shopping_cart_item.rb
+++ lib/active_record/acts/shopping_cart_item.rb
@@ -20,7 +20,7 @@ module ActiveRecord
           self.send :include, ActiveRecord::Acts::ShoppingCartItem::InstanceMethods
           belongs_to :owner, :polymorphic => true
           belongs_to :item, :polymorphic => true
-          monetize :price_cents, with_currency: :price_currency
+          monetize :price_cents, with_model_currency: :price_currency, as: :price
         end
 
         #
